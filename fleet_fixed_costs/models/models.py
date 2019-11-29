8bb609a1a31772bd190f60c1b563e5b3d08b7f3d
LFer <lfferreiradiaz@gmail.com>
2019-11-29 15:51:01 -0300

    Added fleet_fixed_costs app

--- fleet_fixed_costs/models/models.py
+++ fleet_fixed_costs/models/models.py
@@ -8,10 +8,20 @@ class FleetVehicleCost(models.Model):
 
     is_toll = fields.Boolean(string='Costo de Peaje')
 
+    @api.model
+    def create(self, values):
+        context = self._context
+        if 'deault_cost_subtype_id' in context:
+            values['cost_subtype_id'] = context.get('deault_cost_subtype_id', False)
+        fleet = super(FleetVehicleCost, self).create(values)
+        return fleet
+
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
     toll_count = fields.Integer(compute="_compute_count_toll", string='Services')
+
+
 
 
     @api.multi
@@ -19,26 +29,28 @@ class FleetVehicle(models.Model):
         """ This opens log view to view and add new log for this vehicle, groupby default to only show effective costs
             @return: the costs log view
         """
+        cost_subtype_obj = self.env['fleet.service.type']
+        cost_subtype = cost_subtype_obj.search([('name', '=', 'Peaje')], limit=1)
+        toll = False
+        if cost_subtype:
+            toll = cost_subtype.id
+
+
+        # cost_subtype_id
         self.ensure_one()
         copy_context = dict(self.env.context)
         copy_context.pop('group_by', None)
         res = self.env['ir.actions.act_window'].for_xml_id('fleet_fixed_costs', 'fleet_vehicle_fixed_action')
         res.update(
-            context=dict(copy_context, default_vehicle_id=self.id, search_default_parent_false=True),
+            context=dict(copy_context, default_vehicle_id=self.id, search_default_parent_false=True, default_is_toll=True, deault_cost_subtype_id=toll, is_from_toll=True),
             domain=[('vehicle_id', '=', self.id), ('is_toll', '=', True)]
         )
         return res
 
     def _compute_count_toll(self):
-        # Odometer = self.env['fleet.vehicle.odometer']
-        # LogFuel = self.env['fleet.vehicle.log.fuel']
-        # LogService = self.env['fleet.vehicle.log.services']
-        # LogContract = self.env['fleet.vehicle.log.contract']
-        # Cost = self.env['fleet.vehicle.cost']
-        # for record in self:
-        #     record.odometer_count = Odometer.search_count([('vehicle_id', '=', record.id)])
-        #     record.fuel_logs_count = LogFuel.search_count([('vehicle_id', '=', record.id)])
-        #     record.service_count = LogService.search_count([('vehicle_id', '=', record.id)])
-        #     record.contract_count = LogContract.search_count([('vehicle_id', '=', record.id),('state','!=','closed')])
-        #     record.cost_count = Cost.search_count([('vehicle_id', '=', record.id), ('parent_id', '=', False)])
+        Cost = self.env['fleet.vehicle.cost']
+        cost_subtype_obj = self.env['fleet.service.type']
+        cost_subtype = cost_subtype_obj.search([('name', '=', 'Peaje')], limit=1)
+        for record in self:
+            record.toll_count = Cost.search_count([('vehicle_id', '=', record.id), ('parent_id', '=', False), ('cost_subtype_id', '=', cost_subtype.id)])
         return
